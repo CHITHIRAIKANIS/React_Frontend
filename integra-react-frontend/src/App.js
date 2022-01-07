606490840d9cd3ab7ea8beda3d1a945562ddf479
CHITHIRAIKANI.S <88016168+CHITHIRAIKANIS@users.noreply.github.com>
2022-01-07 13:43:24 +0530

    Add files via upload

---
+++ integra-react-frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import RouterComponent from './RouterComponent';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <RouterComponent/>
+    
+    </div>
+  );
+}
+
+export default App;
